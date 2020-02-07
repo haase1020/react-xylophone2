d9db4132fffef6f6cf3443f20a93204b266bdeb5
Mandi Haase <haase1020@gmail.com>
2020-02-07 01:24:15 -0500

    update App.js file

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React from 'react';
 import notes from './assets/notes.js';
 import './App.scss';
 
+const NoteButton =(props) =>{
+  console.log("props", props);
+  return(
+    <button onClick={()=> new Audio(props.note.file).play()}>
+
+    </button>
+  );
+};
+
 function App() {
   return (
     <div className="page">
       <h1>Create your react xylophone</h1>
       <div className="xylophone">
         {/* I am placeholder buttons, please create me using a map, and with your own component */}
-        <button>c</button>
+        <NoteButton note={notes[0]}/>
+        <button onClick={()=> new Audio(notes)}>c</button>
         <button>d1</button>
         <button>e1</button>
         <button>f</button>
