c8a0850bf302fc7214ae05a1534a705383cc6e94
Mandi Haase <haase1020@gmail.com>
2020-02-07 07:55:09 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -8,34 +8,52 @@ const NoteButton =(props) =>{
     className="note-button"
     style={{ height: (200 -(15 * props.index)) + 'px' }}
     onClick={()=> {
-      props.setPlayedNotes([...props.playedNotes, props.note]);
+      // props.setPlayedNotes([...props.playedNotes, props.note]);
+      props.addPlayedNote(props.note);
       console.log(props.playedNotes);
       new Audio(props.note.file).play()
     }}>
+      <div className="circle" />
       {props.note.name}
+      <div className="circle" />
     </button>
   );
 };
 
 function App() {
   const [playedNotes, setPlayedNotes] = useState([]);
+
+  const replayNotes = () => {
+    playedNotes.map((note, index) => {
+      window.setTimeout(()=> new Audio(note.file).play(), 500 * index);
+    });
+  };
+const resetNotes = () =>{
+  setPlayedNotes([]);
+};
+
+const addPlayedNote =(note) => {
+  setPlayedNotes([...playedNotes, note]);
+};
+
   return (
     <div className="page">
       <h1>Create your react xylophone</h1>
       <div className="xylophone">
         {/* I am placeholder buttons, please create me using a map, and with your own component */}
         {notes.map((note,index) =>(
-        <NoteButton 
+        <NoteButton
+        index={index} 
         playedNotes={playedNotes}
-        setPlayedNotes={setPlayedNotes}
+        addPlayedNote={addPlayedNote}
         key={note.name}
         note={note} 
         />
         ))}
       </div>
       {JSON.stringify(playedNotes)}
-      <button onClick={()=> playedNotes.map(note => new Audio(note.file).play())}>Replay</button>
-      <button onClick={() => setPlayedNotes([])}>Clear</button>
+      <button onClick={()=> replayNotes()}>Replay</button>
+      <button onClick={() => resetNotes()}>Clear</button>
   
       {/* <button>Clear</button> */}
     </div>
