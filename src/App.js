20e9a85861e6c9b8270080d665bd35b81782cd2f
Mandi Haase <haase1020@gmail.com>
2020-02-07 06:22:37 -0500

    adduseState to App.js and add some styling to App.scss

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
-import React from 'react';
+import React, { useState }from 'react';
 import notes from './assets/notes.js';
 import './App.scss';
 
 const NoteButton =(props) =>{
-  console.log("props", props);
   return(
-    <button onClick={()=> new Audio(props.note.file).play()}>
-
+    <button 
+    className="note-button"
+    onClick={()=> {
+      props.setPlayedNotes([...props.playedNotes, props.note]);
+      console.log(props.playedNotes);
+      new Audio(props.note.file).play()
+    }}>
+      {props.note.name}
     </button>
   );
 };
 
 function App() {
+  const [playedNotes, setPlayedNotes] = useState([]);
   return (
     <div className="page">
       <h1>Create your react xylophone</h1>
       <div className="xylophone">
         {/* I am placeholder buttons, please create me using a map, and with your own component */}
-        <NoteButton note={notes[0]}/>
-        <button onClick={()=> new Audio(notes)}>c</button>
-        <button>d1</button>
-        <button>e1</button>
-        <button>f</button>
-        <button>g</button>
-        <button>a</button>
-        <button>b</button>
-        <button>c2</button>
+        
+        {notes.map(note => (
+        <NoteButton 
+        playedNotes={playedNotes}
+        setPlayedNotes={setPlayedNotes}
+        key={note.name}
+        note={note} 
+        />
+        ))}
       </div>
       {/* <button>Replay</button> */}
       {/* <button>Clear</button> */}
